d8887d193c28d51d8520f983c272582a3f1641b2
Julian Bernard <julian.bernard@baxter-academy.org>
2016-01-21 18:34:00 -0500

    Testing adding NavX to gradle and idea, also sending IMU data to dashboard.

--- src/main/java/org/usfirst/frc/team5687/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team5687/robot/Robot.java
@@ -1,7 +1,9 @@
 
 package org.usfirst.frc.team5687.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -9,6 +11,8 @@ import org.usfirst.frc.team5687.robot.commands.ExampleCommand;
 import org.usfirst.frc.team5687.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.kauailabs.navx.frc.AHRS;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,6 +25,8 @@ public class Robot extends IterativeRobot {
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
+
+    public static AHRS ahrs;
 
     Command autonomousCommand;
     SendableChooser chooser;
@@ -35,6 +41,16 @@ public class Robot extends IterativeRobot {
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
+
+        try {
+            ahrs = new AHRS(SPI.Port.kMXP);
+            DriverStation.reportError(String.format("Connected to navX MXP with FirmwareVersion %1$s", ahrs.getFirmwareVersion()), false);
+            SmartDashboard.putString(   "FirmwareVersion",      ahrs.getFirmwareVersion());
+
+        } catch (Exception ex) {
+            DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+            ahrs = null;
+        }
     }
 	
 	/**
@@ -95,14 +111,65 @@ public class Robot extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
+    public void teleopPeriodic()
+    {
+        sendIMUData();
         Scheduler.getInstance().run();
     }
-    
+
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
         LiveWindow.run();
+    }
+
+    protected void sendIMUData() {
+           /* Display 6-axis Processed Angle Data                                      */
+        SmartDashboard.putBoolean(  "IMU_Connected",        ahrs.isConnected());
+        SmartDashboard.putBoolean(  "IMU_IsCalibrating",    ahrs.isCalibrating());
+        SmartDashboard.putNumber(   "IMU_Yaw",              ahrs.getYaw());
+        SmartDashboard.putNumber(   "IMU_Pitch",            ahrs.getPitch());
+        SmartDashboard.putNumber(   "IMU_Roll",             ahrs.getRoll());
+
+           /* Display tilt-corrected, Magnetometer-based heading (requires             */
+           /* magnetometer calibration to be useful)                                   */
+
+        SmartDashboard.putNumber(   "IMU_CompassHeading",   ahrs.getCompassHeading());
+
+           /* Display 9-axis Heading (requires magnetometer calibration to be useful)  */
+        SmartDashboard.putNumber(   "IMU_FusedHeading",     ahrs.getFusedHeading());
+
+           /* These functions are compatible w/the WPI Gyro Class, providing a simple  */
+           /* path for upgrading from the Kit-of-Parts gyro to the navx MXP            */
+
+        SmartDashboard.putNumber(   "IMU_TotalYaw",         ahrs.getAngle());
+        SmartDashboard.putNumber(   "IMU_YawRateDPS",       ahrs.getRate());
+
+           /* Display Processed Acceleration Data (Linear Acceleration, Motion Detect) */
+
+        SmartDashboard.putNumber(   "IMU_Accel_X",          ahrs.getWorldLinearAccelX());
+        SmartDashboard.putNumber(   "IMU_Accel_Y",          ahrs.getWorldLinearAccelY());
+        SmartDashboard.putBoolean(  "IMU_IsMoving",         ahrs.isMoving());
+        SmartDashboard.putBoolean(  "IMU_IsRotating",       ahrs.isRotating());
+
+           /* Display estimates of velocity/displacement.  Note that these values are  */
+           /* not expected to be accurate enough for estimating robot position on a    */
+           /* FIRST FRC Robotics Field, due to accelerometer noise and the compounding */
+           /* of these errors due to single (velocity) integration and especially      */
+           /* double (displacement) integration.                                       */
+
+        SmartDashboard.putNumber(   "Velocity_X",           ahrs.getVelocityX());
+        SmartDashboard.putNumber(   "Velocity_Y",           ahrs.getVelocityY());
+        SmartDashboard.putNumber(   "Displacement_X",       ahrs.getDisplacementX());
+        SmartDashboard.putNumber(   "Displacement_Y",       ahrs.getDisplacementY());
+
+
+
+
+           /* Connectivity Debugging Support                                           */
+        SmartDashboard.putNumber(   "IMU_Byte_Count",       ahrs.getByteCount());
+        SmartDashboard.putNumber(   "IMU_Update_Count",     ahrs.getUpdateCount());
+
     }
 }
