ade28c795285c69d2a810d4e3a70563983f1dae0
wilstenholme <woraman.lo@gmail.com>
2016-01-22 13:24:40 -0500

    Assign enum values to Gamepad

--- src/main/java/org/usfirst/frc/team5687/robot/utils/Gamepad.java
+++ src/main/java/org/usfirst/frc/team5687/robot/utils/Gamepad.java
@@ -14,29 +14,48 @@ public class Gamepad extends Joystick{
 	 * Enumeration for the various analog axes
 	 */
     public static enum Axes {
-        LEFT_X,
-        LEFT_Y,
-        LEFT_TRIGGER,
-        RIGHT_TRIGGER,
-        RIGHT_X,
-        RIGHT_Y,
-        D_PAD_HORIZ
+        LEFT_X(0),
+        LEFT_Y(1),
+        LEFT_TRIGGER(2),
+        RIGHT_TRIGGER(3),
+        RIGHT_X(4),
+        RIGHT_Y(5),
+        D_PAD_HORIZONTAL(6),
+        D_PAD_VERTICAL(7);
+
+        private final int axisValue;
+        Axes(int axisValue) {
+            this.axisValue = axisValue;
+        }
+
+        public int getAxisValue() {
+            return axisValue;
+        }
     }
 
     /*
      * Enumeration for the various buttons
      */
     public static enum Buttons {
-        A,
-        B,
-        X,
-        Y,
-        LB,
-        RB,
-        BACK,
-        START,
-        LEFT_STICK,
-        RIGHT_STICK
+        A(0),
+        B(1),
+        X(2),
+        Y(3),
+        LB(4),
+        RB(5),
+        BACK(6),
+        START(7),
+        LEFT_BUTTON(8),
+        RIGHT_BUTTON(9);
+
+        private final int buttonValue;
+        Buttons(int buttonValue) {
+            this.buttonValue = buttonValue;
+        }
+
+        public int getButtonValue() {
+            return buttonValue;
+        }
     }
 
     /*
