3e83ce4f52a114126c4be5505670d64b00d22b0e
wilstenholme <woraman.lo@gmail.com>
2016-01-22 11:05:21 -0500

    Add applyDeadband to Helpers

--- src/main/java/org/usfirst/frc/team5687/robot/utils/Helpers.java
+++ src/main/java/org/usfirst/frc/team5687/robot/utils/Helpers.java
@@ -3,8 +3,21 @@ package org.usfirst.frc.team5687.robot.utils;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class Helpers{
+    /*
+     * Logs an action to Drive Station
+     * @param message the message to send to Drive Station
+     */
     protected void LogAction(String message) {
-        // Logs the action to Drive Station
         DriverStation.reportError(message + "\r\n", false);
+    }
+
+    /*
+	 * This applies a deadband value
+	 * @param input raw value from joystick
+	 * @param deadband the deadband threshold
+	 * @return double the adjusted value
+	 */
+    public static double applyDeadband(double input, double deadband){
+        return (Math.abs(input) >= Math.abs(deadband)) ? input : 0;
     }
 }
