30b35c4374a6ddef8a954e5daac4e0c6e4fcacc1
BenBernardCIS <benbernard@cliffislandsoftware.com>
2016-01-24 12:28:02 -0500

    Renaming tankDrive.

--- src/main/java/org/usfirst/frc/team5687/robot/commands/DriveWith2Joysticks.java
+++ src/main/java/org/usfirst/frc/team5687/robot/commands/DriveWith2Joysticks.java
@@ -39,7 +39,7 @@ public class DriveWith2Joysticks extends Command {
      * @see edu.wpi.first.wpilibj.command.Command#execute()
      */
     protected void execute() {
-        driveTrain.s_tankDrive(oi.getLeftspeed(), oi.getRightspeed());
+        driveTrain.tankDrive(oi.getLeftspeed(), oi.getRightspeed());
     }
 
     /*
--- src/main/java/org/usfirst/frc/team5687/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team5687/robot/subsystems/DriveTrain.java
@@ -31,7 +31,7 @@ public class DriveTrain extends Subsystem {
     }
 
 
-    public void s_tankDrive(double leftSpeed, double rightSpeed){
+    public void tankDrive(double leftSpeed, double rightSpeed){
 
         // Limit change in leftSpeed to +/- ACCELERATION_CAP
         leftSpeed = Math.min(leftSpeed, leftMotor.get() + Constants.Limits.ACCELERATION_CAP);
