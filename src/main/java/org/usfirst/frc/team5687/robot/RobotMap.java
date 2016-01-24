75ee78e7295dffa35724406134f7b6f77f700a87
Ben Bernard <benbernard@cliffislandsoftware.com>
2016-01-24 11:16:22 -0500

    Updating RobotMap to reflect victor positions. Setting default command for DriveTrain.

--- src/main/java/org/usfirst/frc/team5687/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team5687/robot/RobotMap.java
@@ -11,7 +11,7 @@ public class RobotMap {
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
     public static int leftMotor = 0;
-    public static int rightMotor = 1;
+    public static int rightMotor = 3;
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
