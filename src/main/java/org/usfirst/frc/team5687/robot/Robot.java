89e5bdc5d8b2239d67e9ff0620cda3f5e59cf486
Maya <maya.schmidt@baxter-academy.org>
2016-02-03 18:49:48 -0500

    <p> deleted to please Travis CI

--- src/main/java/org/usfirst/frc/team5687/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team5687/robot/Robot.java
@@ -95,7 +95,6 @@ public class Robot extends IterativeRobot {
      * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
      * Dashboard, remove all of the chooser code and uncomment the getString code to get the auto name from the text box
      * below the Gyro
-     * <p/>
      * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
      * or additional comparisons to the switch structure below with additional strings and commands.
      */
