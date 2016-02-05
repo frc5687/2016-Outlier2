ff67de3ccce8407c259b1bf459779ccb2381a7d1
Maya <maya.schmidt@baxter-academy.org>
2016-02-05 14:18:27 -0500

    Encoders is copy-pasted into the Constants class to please DriveTrain without risking another pull from master into branch.

--- src/main/java/org/usfirst/frc/team5687/robot/Constants.java
+++ src/main/java/org/usfirst/frc/team5687/robot/Constants.java
@@ -35,4 +35,26 @@ public class Constants {
          */
         public static final double SENSITIVITY_FACTOR = 0.5;
     }
+    public class Encoders {
+        public class Defaults {
+            public static final boolean REVERSED = false;
+            public static final int SAMPLES_TO_AVERAGE = 20;
+            public static final int PULSES_PER_ROTATION = 2048;
+            public static final double WHEEL_DIAMETER = 13;
+            public static final double INCHES_PER_ROTATION = Math.PI * WHEEL_DIAMETER;
+            public static final double SCALAR_RATIO = 1.4;
+            public static final double INCHES_PER_PULSE = INCHES_PER_ROTATION * SCALAR_RATIO / PULSES_PER_ROTATION ;
+            public static final double MAX_PERIOD = 5;
+        }
+
+        public class RightDrive {
+            public static final boolean REVERSED = Defaults.REVERSED;
+            public static final double INCHES_PER_PULSE = Defaults.INCHES_PER_PULSE;
+        }
+
+        public class LeftDrive {
+            public static final boolean REVERSED = true;
+            public static final double INCHES_PER_PULSE = Defaults.INCHES_PER_PULSE;
+        }
+    }
 }
