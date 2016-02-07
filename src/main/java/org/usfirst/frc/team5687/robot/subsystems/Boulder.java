cbb53dda19fe61c0f6326922ad05f788b317102c
wilstenholme <woraman.lo@gmail.com>
2016-02-07 17:14:01 -0500

    Update command descriptions before removing

--- src/main/java/org/usfirst/frc/team5687/robot/subsystems/Boulder.java
+++ src/main/java/org/usfirst/frc/team5687/robot/subsystems/Boulder.java
@@ -28,61 +28,53 @@ public class Boulder extends Subsystem {
     }
 
     public void intake() {
-
         //Start Conditions: Hopper sensor detects NO boulder in hopper
         //                  Check for shooter wheels not spinning
         //Do What:          Run Intake roller IN
-        //End Conditions:   Hopper sensor detects a boulder in hopper
-
+        //End Conditions:   Hopper sensor detects a boulder in hopper, sets hasBoulder flag
     }
 
-    public void bowlLow () {
-
-        //Start Conditions: Hopper sensor detects boulder in hopper
+    public void bowlLow() {
+        //Start Conditions: if hasBoulder(), (hopper sensor detects boulder in hopper)
         //                  IMU Detects proper angle (not on floor, as that is too far away from Goal)
         //                  Vision tracking detects goal (if able) or distance (if able)
-        //Do What:          Run Intake roller OUT
-        //End Conditions:   Hopper sensor detects NO boulder in hopper and(or?) Drivers view Boulder enter Goal
+        //Do What:          Run Intake roller reverse
+        //End Conditions:   Hopper sensor detects NO boulder in hopper and wait for a time and/(or?) Drivers view Boulder enter Goal
+        //                  Reset(?)
     }
 
     public void prime() {
-
         //Should run automatically when IMU detects robot has navigated a defense into opponent's courtyard
 
-        //Start Conditions: Hopper sensor detects boulder in hopper
-        //Do What:          Run Intake roller OUT, partially
+        //Start Conditions: Hopper sensor detects boulder in hopper, robot is level
+        //Do What:          Run Intake roller 1/4 turn reverse, using a constant
         //                  (once complete), gets wheel
-        //End Conditions:   ShootHigh command is run
+        //End Conditions:   sets isPrimed flag, shootHigh command is ready
     }
 
     public void shootHigh() {
-
-        //Start Conditions: Hopper sensor detects NO boulder in contact with shooter wheels
+        //Start Conditions: isPrimed, Hopper sensor detects NO boulder in contact with shooter wheels
         //                  Robot is not moving significantly (or at all?)
-        //Do What:          Run Intake roller IN
-        //End Conditions:   Encoder detects drop in shooter wheels RPM, (science) ?
+        //Do What:          Run Intake roller forward
+        //End Conditions:   Encoder detects drop in shooter wheels RPM, or wait for time
+        //                  Reset(?)
     }
 
     public void cancel() {
-
         //Do What:         Cancel a command
         //End Conditions:  Command has been canceled
     }
 
     public void lock() {
-
         //Should run automatically if IMU detects a collision
 
         //Do What:          Prevent ShootHigh or ShootLow command from executing
         //End Conditions:   Robot stops moving
-
     }
 
     public void reset() {
-
         //Do What:          Cut power to shooter wheels
         //End Conditions:   No boulder in hopper, shooter wheels at 0 rpm
-        //Then do what:     (if we go  with flag-based) Clear Primed and HasBoulder flags
-
+        //Then do what:     Clear primed and hasBoulder flags
     }
 }
