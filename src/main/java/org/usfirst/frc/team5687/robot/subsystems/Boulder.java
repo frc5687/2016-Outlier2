a623b767bad6e2606d0d921491b02d41b40819c3
Ryan Foley <rtfoley@gmail.com>
2016-02-07 22:45:29 -0500

    Add javadoc and clean imports for boulder system

--- src/main/java/org/usfirst/frc/team5687/robot/subsystems/Boulder.java
+++ src/main/java/org/usfirst/frc/team5687/robot/subsystems/Boulder.java
@@ -1,24 +1,35 @@
 package org.usfirst.frc.team5687.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team5687.robot.RobotMap;
+import org.usfirst.frc.team5687.robot.commands.RunShooterManually;
 
 public class Boulder extends Subsystem {
-    
-    VictorSP shooterWheel;
-    Encoder shooterRotation;
 
+    /**
+     * Victor speed controller for the shooter motor
+     */
+    private VictorSP shooterWheel;
 
-    public void  Boulder() {
+    /**
+     * Constructor
+     */
+    public  Boulder() {
         shooterWheel = new VictorSP(RobotMap.shooterWheelMotor);
     }
 
+    /**
+     * Set the shooter wheel speed
+     * @param speed the desired speed control value
+     */
     public void setShooterSpeed(double speed) {
         shooterWheel.set(speed);
     }
 
+    /**
+     * Set the default command for the shooter subsystem
+     */
     public void initDefaultCommand() {
         setDefaultCommand(new RunShooterManually());
     }
