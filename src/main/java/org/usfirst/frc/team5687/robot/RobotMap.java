982a46dd3693514ce2f87db97c397f419554b53d
wilstenholme <woraman.lo@gmail.com>
2016-02-09 15:33:48 -0500

    Update RobotMap for constants

--- src/main/java/org/usfirst/frc/team5687/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team5687/robot/RobotMap.java
@@ -12,25 +12,25 @@ public class RobotMap {
      * Drive Train ports
      */
     public static class Drive {
-        public static int LEFT_MOTORS = 0;
-        public static int RIGHT_MOTORS = 1;
+        public static final int LEFT_MOTORS = 0;
+        public static final int RIGHT_MOTORS = 1;
 
-        public static int LEFT_ENCODER_CHANNEL_A = 8;
-        public static int LEFT_ENCODER_CHANNEL_B = 9;
-        public static int RIGHT_ENCODER_CHANNEL_A = 6;
-        public static int RIGHT_ENCODER_CHANNEL_B = 7;
+        public static final int LEFT_ENCODER_CHANNEL_A = 8;
+        public static final int LEFT_ENCODER_CHANNEL_B = 9;
+        public static final int RIGHT_ENCODER_CHANNEL_A = 6;
+        public static final int RIGHT_ENCODER_CHANNEL_B = 7;
 
-        public static int PDP_LEFT_MOTOR1 = 0;
-        public static int PDP_LEFT_MOTOR2 = 1;
-        public static int PDP_RIGHT_MOTOR1 = 2;
-        public static int PDP_RIGHT_MOTOR2 = 3;
+        public static final int PDP_LEFT_MOTOR1 = 0;
+        public static final int PDP_LEFT_MOTOR2 = 1;
+        public static final int PDP_RIGHT_MOTOR1 = 2;
+        public static final int PDP_RIGHT_MOTOR2 = 3;
     }
 
     /**
      * Shooter ports
      */
     public static class Shooter {
-        public static int WHEEL_MOTOR = 2;
+        public static final int WHEEL_MOTOR = 2;
     }
 
 }
