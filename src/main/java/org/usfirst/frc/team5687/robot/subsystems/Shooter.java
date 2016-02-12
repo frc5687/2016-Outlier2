ff267a1d336ec5a67ccbea93fa0c11ae7c916a7e
JCharante <ralphcharante@gmail.com>
2016-02-12 15:00:37 -0500

    Logging how long the shooter wheel has been spinning instead. Removed Encoder code

--- src/main/java/org/usfirst/frc/team5687/robot/subsystems/Shooter.java
+++ src/main/java/org/usfirst/frc/team5687/robot/subsystems/Shooter.java
@@ -14,14 +14,10 @@ public class Shooter extends Subsystem {
      * Victor speed controller for the shooter motor
      */
     private VictorSP wheelMotor;
-    private Encoder shooterWheelEncoder;
+    private Double SpinTime;
 
-    /**
-     * Constructor
-     */
     public Shooter() {
         wheelMotor = new VictorSP(RobotMap.shooterWheelMotor);
-        shooterWheelEncoder = initializeEncoder(RobotMap.shooterWheelEncoderChannelA, RobotMap.shooterWheelEncoderChannelB, Constants.Encoders.ShooterWheel.REVERSED, Constants.Encoders.ShooterWheel.INCHES_PER_PULSE);
     }
 
     /**
@@ -30,26 +26,17 @@ public class Shooter extends Subsystem {
      */
     public void setSpeed(double speed) {
         wheelMotor.set(speed);
-        SmartDashboard.putNumber("ShooterWheel speed", getShooterWheelRPM());
+        if (speed != 0) {
+            SpinTime = SmartDashboard.getNumber("Shooter Spin Up Time", 0);
+            SpinTime += 0.02;
+            SmartDashboard.putNumber("Shooter Spin Up Time", SpinTime);
+        } else {
+            SmartDashboard.putNumber("Shooter Spin Up Time", 0);
+        }
     }
 
-    /**
-     * Set the default command for the shooter subsystem
-     */
     public void initDefaultCommand() {
         setDefaultCommand(new RunShooterManually());
     }
 
-    private Encoder initializeEncoder(int channelA, int channelB, boolean reversed, double distancePerPulse) {
-        Encoder encoder = new Encoder(channelA, channelB, reversed, Encoder.EncodingType.k4X);
-        encoder.setDistancePerPulse(distancePerPulse);
-        encoder.setMaxPeriod(Constants.Encoders.Defaults.MAX_PERIOD);
-        encoder.setSamplesToAverage(Constants.Encoders.Defaults.SAMPLES_TO_AVERAGE);
-        encoder.reset();
-        return encoder;
-    }
-
-    public double getShooterWheelRPM() {
-        return shooterWheelEncoder.getRate() / (Constants.Encoders.ShooterWheel.PULSES_PER_ROTATION * Constants.Encoders.ShooterWheel.INCHES_PER_PULSE);
-    }
 }
