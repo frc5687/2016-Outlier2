81adc4933c7c2bcbdd6ac4f4298f108fa0cd7fb7
Maya <maya.schmidt@baxter-academy.org>
2016-02-23 17:52:34 -0500

    Steps 1-5 of outline completed. Need to fix PIDcontroller object and complete step 6.

--- src/main/java/org/usfirst/frc/team5687/robot/commands/AutoAlign.java
+++ src/main/java/org/usfirst/frc/team5687/robot/commands/AutoAlign.java
@@ -3,8 +3,6 @@ package org.usfirst.frc.team5687.robot.commands;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team5687.robot.Robot;
-
 import static org.usfirst.frc.team5687.robot.Robot.imu;
 import static org.usfirst.frc.team5687.robot.Robot.driveTrain;
 
--- src/main/java/org/usfirst/frc/team5687/robot/commands/AutoDrive.java
+++ src/main/java/org/usfirst/frc/team5687/robot/commands/AutoDrive.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team5687.robot.commands;
 
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team5687.robot.OI;
 import org.usfirst.frc.team5687.robot.Robot;
 import org.usfirst.frc.team5687.robot.subsystems.DriveTrain;
@@ -14,8 +17,9 @@ import java.util.Date;
  * Eventually we will want to add distance-based options.
  */
 
-public class AutoDrive extends Command {
+public class AutoDrive extends Command implements PIDOutput{
     DriveTrain driveTrain = Robot.driveTrain;
+    AHRS imu = Robot.imu;
     OI oi = Robot.oi;
     private long end = 0;
     private int timeToDrive = 0;
@@ -24,6 +28,16 @@ public class AutoDrive extends Command {
     private double leftSpeed = 0;
     private double inchesDriven = 0;
     private boolean driveByTime;
+
+    private static final double kP = 0.3;
+    private static final double kI = 0.05;
+    private static final double kD = 0.1;
+    private static final double kF = 0.1;//Q: What is this for?
+    private static final double rotationDeadband = 0.01;
+    private static final double kToleranceDegrees = 2.0f;
+    private double rotateToAngleRate = 0; //Q: how does the PIDcontroller object know to use this variable?
+    private double targetAngle = 0;
+    private double currentAngle = 0;
 
     //Drive based on time
     public AutoDrive(double speed, int timeToDrive) {
@@ -51,13 +65,21 @@ public class AutoDrive extends Command {
     protected void initialize() {
         DriverStation.reportError(String.format("Accelerating to %1$f\n", rightSpeed), false);
         end = (new Date()).getTime() + timeToDrive;
-
+        targetAngle = imu.getYaw();
         driveTrain.resetDriveEncoders();
+        turnController.setInputRange(-180.0f,  180.0f);
+        turnController.setOutputRange(-0.5, 0.5);
+        turnController.setAbsoluteTolerance(kToleranceDegrees);
+        turnController.setContinuous(true);
+        turnController.setSetpoint(targetAngle);
+        turnController.enable();
     }
 
     @Override
     protected void execute() {
+
         driveTrain.tankDrive(leftSpeed, rightSpeed);
+
     }
 
     @Override
@@ -82,5 +104,14 @@ public class AutoDrive extends Command {
     @Override
     protected void interrupted() {
 
+    }
+
+    @Override
+    public void pidWrite(double output) {
+        synchronized (this) {
+            SmartDashboard.putNumber("AutoAlign/PID Output", output);
+            DriverStation.reportError("AutoAlign/PID Output " + output, false);
+            rotateToAngleRate = output;
+        }
     }
 }
