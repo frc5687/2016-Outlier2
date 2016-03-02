757278003bdae8ee674c2ed8168f858b485a4f97
Maya <maya.schmidt@baxter-academy.org>
2016-03-01 19:30:06 -0500

    Currently working to correct use of RunArmsManually by creating an AutoRunArms command.

---
+++ src/main/java/org/usfirst/frc/team5687/robot/commands/AutoRunArms.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5687.robot.commands;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team5687.robot.Constants;
+import org.usfirst.frc.team5687.robot.OI;
+import org.usfirst.frc.team5687.robot.Robot;
+import org.usfirst.frc.team5687.robot.subsystems.Arms;
+
+/**
+ * Created by Baxter on 3/1/2016.
+ */
+public class AutoRunArms {
+    /**
+     * Command for basic manual-control of the arms
+     * Created by Maya on 2/10/2016.
+     */
+    public static class AutoRunArms extends Command {
+    }
