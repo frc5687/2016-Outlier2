4c50f540244524fcb5e5c19b96154d8af1bdf175
wilstenholme <woraman.lo@gmail.com>
2016-03-02 19:21:25 -0500

    Reduce auto traversing distance for first position

--- src/main/java/org/usfirst/frc/team5687/robot/commands/AutoTraverseOnly.java
+++ src/main/java/org/usfirst/frc/team5687/robot/commands/AutoTraverseOnly.java
@@ -51,8 +51,8 @@ public class AutoTraverseOnly extends CommandGroup {
         switch (position){
             case 1:
 
-                // Run forward 112 inches
-                addSequential(new AutoDrive(.7, 124.0f));
+                // Run forward 100 inches
+                addSequential(new AutoDrive(.7, 100.0f));
 
                 // Turn towards the tower
                 addSequential(new AutoAlign(50));
