0d4035e65b66892a360a5556cd93d726c6e05585
wilstenholme <woraman.lo@gmail.com>
2016-04-15 14:11:12 -0400

    Tune auto traverse and turn distances

--- src/main/java/org/usfirst/frc/team5687/robot/Constants.java
+++ src/main/java/org/usfirst/frc/team5687/robot/Constants.java
@@ -47,7 +47,7 @@ public class Constants {
 
     public class Autonomous {
         public static final double MIN_AUTO_TRAVERSE_ANGLE = 7; // the minimum angle the robot will pitch when traversing a defense
-        public static final double MAX_AUTO_TRAVERSE_SECONDS = 4; // the maximum time the robot should need to travers a defense in Autonomous,
+        public static final double MAX_AUTO_TRAVERSE_SECONDS = 7; // the maximum time the robot should need to travers a defense in Autonomous,
         
     }
 
